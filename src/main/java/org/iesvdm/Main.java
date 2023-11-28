79c64188fff3b6718935fb12f3b7f5eb33de5c9a
Guillermo <guille@example.com>
2023-11-28 12:29:52 +0100

    push para activar jenkins

--- src/main/java/org/iesvdm/Main.java
+++ src/main/java/org/iesvdm/Main.java
@@ -9,82 +9,8 @@ import java.time.Month;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        LocalDate fecha = LocalDate.of(2023, Month.NOVEMBER, 1); // primer dia del mes
 
-        // Array dias
-
-        String[] diasSemana = {"Lun", "Mar", "Mier", "Jue", "Vie", "Sab", "Dom"};
-
-        // impresion tabla
-
-        // <table>
-
-        System.out.println("<table>");
-
-        // Impresion cabecera
-
-        // <tr>
-
-        System.out.println("<tr>");
-
-        for(String dia : diasSemana){
-
-            System.out.println("<th>" + dia + "</th>");
-
-        }
-
-        //</tr>
-
-        System.out.println("</tr>");
-
-        // Obtener dia que empieza el mes
-
-        int numeroPrimerDia = fecha.getDayOfWeek().getValue();
-
-        int contadorDias = 1;
-        int diaCalendario = 1;
-        int diaFinalMes = fecha.lengthOfMonth();
-        // cambio
-
-        while(contadorDias <= diaFinalMes + numeroPrimerDia){
-
-            // <tr>
-            System.out.println("<tr>");
-
-            // Imprimir semanas
-            for(int i = 1; i <= 7; i++){
-
-                // <td>
-                System.out.println("<td>");
-
-                // Si debe estar en blanco
-                if(contadorDias < numeroPrimerDia || contadorDias > diaFinalMes){
-
-                    System.out.println("  ");
-
-                }else{ // si debe imprimir dia
-
-                    System.out.println(diaCalendario);
-                    diaCalendario++;
-                }
-
-                // </td>
-                System.out.println("</td>");
-
-
-                // Incremento dias
-                contadorDias++;
-            }
-
-            // </tr>
-            System.out.println("</tr>");
-
-        }
-
-        // </table>
-        System.out.println("</table>");
-
-
+        System.out.println("Hola mundo Guillermo");
 
     }
 }
